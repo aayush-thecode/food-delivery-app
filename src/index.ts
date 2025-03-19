7f2b087129f5432ca1a7edabd9a6e8d5c9be6777
Aayush Pradhan <muma5581@gmail.com>
2025-03-19 22:32:31 +0545

    chat bot  route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import 'dotenv/config'
 import userRoutes from './routes/user.routes'
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
 import connectDatabase from './config/databse.config';
-import { CustomError } from './middleware/errorhandeler.middleware';
 import foodTypeRoutes from './routes/foodtype.routes'
-import { Together } from "together-ai"
-import chatRoutes from './routes/chat.routes'
+import chatRoutes from './routes/chatbot.routes'
 import path from 'path';
 import categoryRoutes from './routes/category.routes'
 import reviewRoutes from './routes/review.routes'
@@ -13,10 +11,10 @@ import reviewRoutes from './routes/review.routes'
 
 const app = express();
 
-const Db_uri = process.env.Db_uri || ''
+const Db_URI = process.env.Db_URI || ''
 const PORT = process.env.PORT || 8080;
 
-connectDatabase(Db_uri)
+connectDatabase(Db_URI)
 
 
 //using middleware 
@@ -25,62 +23,13 @@ app.use('/api/uploads',express.static(path.join(__dirname,'../', 'uploads')))
 
 
 //using routes
-app.use('/api/user/', userRoutes)
+app.use('/api/user', userRoutes)
 app.use('/api/foodtype', foodTypeRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/review', reviewRoutes)
 
 
-
-//call the function with the promopt 
-const together = new Together ({
-  apiKey : process.env.TOGETHER_AI_API_KEY as string, // type assertion
-});
-
-//define an async function to get chatbox response 
-
-async function getChatResponse(prompt: string): Promise<void> {
-  try {
-    const response = await together.chat.completions.create ({
-      messages: [{ role: 'user', content: prompt }],
-      model: "meta-llama/Llama-3.3-70B-Instruct-Turbo",
-    });
-
-    console.log(response.choices[0].message?.content);
-
-  } catch (error) {
-
-    console.log("error fetching response:", error);
-  }
-}
-
-//initialize together API with API key 
-
-getChatResponse("what are some fun things to do in food app?")
-
-//handle not found path 
-app.all('*', (req:Request, res:Response, next:NextFunction) => {
-
-    const message = `cannot ${req.method} on ${req.originalUrl}`
-
-    const error = new CustomError(message, 404)
-    next(error) 
-})
-
-//error handler
-
-app.use((error:any, req: Request, res:Response, next:NextFunction) => {
-    const statusCode = error.statusCode || 500
-    const message = error.message || 'something went wrong!'
-    const status = error.status || 'error'
-
-    res.status(statusCode).json ({
-        status: status,
-        success: false,
-        message: message
-    })
-})
 
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`)
